1ac96659ce4a41acf3d8cc7d9868d653990c701d
Michael Pogoda <do0dlez1991@gmail.com>
2013-07-24 10:32:46 +0300

    Updated tmux-dashboard for desktop:
     * renamed window mplayer -> media.
     * show log of Xorg instead of daemon.

--- tmux.sh
+++ tmux.sh
@@ -5,12 +5,16 @@ tmux has-session -t $SESSION_NAME
 if [ $? -ne 0 ]
 then
   tmux new-session -d -s $SESSION_NAME -n logs 'sudo tail -f /var/log/messages'
-  tmux split-window -t $SESSION_NAME 'sudo tail -f /var/log/daemon.log'
-  tmux new-window -c ~               -n update  -t $SESSION_NAME
-  tmux new-window                    -n htop    -t $SESSION_NAME htop
+  tmux split-window -t $SESSION_NAME 'sudo tail -f /var/log/Xorg.0.log'
+  tmux new-window -c /data/downloads -n media  -t $SESSION_NAME
+  tmux new-window -c ~               -n update -t $SESSION_NAME
+  tmux new-window                    -n ncmpc  -t $SESSION_NAME ncmpc
+  tmux new-window                    -n htop   -t $SESSION_NAME htop
+  tmux set-window-option -t $SESSION_NAME:media monitor-activity off
   tmux set-window-option -t $SESSION_NAME:update  allow-rename     off
   tmux set-window-option -t $SESSION_NAME:update  monitor-activity off
   tmux set-window-option -t $SESSION_NAME:update  monitor-silence  30
+  tmux set-window-option -t $SESSION_NAME:ncmpc   monitor-activity off
   tmux set-window-option -t $SESSION_NAME:htop    monitor-activity off
 fi
 
